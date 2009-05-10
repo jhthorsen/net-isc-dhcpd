fa7d97f1040fa8ddb45b0b9db3a19672f6d30039
Jan Henning Thorsen <git@flodhest.net>
2009-05-10 22:42:00 +0200

    add KeyValue support (last resort)

---
+++ lib/Net/DHCPd/Config/KeyValue.pm
@@ -0,0 +1,91 @@
+package Net::DHCPd::Config::KeyValue;
+
+=head1 NAME
+
+Net::DHCPd::Config::KeyValue - Misc option config parameter
+
+=cut
+
+use Moose;
+
+with 'Net::DHCPd::Config::Role';
+
+=head1 OBJECT ATTRIBUTES
+
+=head2 regex
+
+See L<Net::DHCPd::Config::Role>.
+
+=cut
+
+has '+regex' => (
+    default => sub { qr{^\s* ([\w-]+) \s (.*) ;}x },
+);
+
+=head2 name
+
+ $string = $self->name;
+
+Name of the option.
+
+=cut
+
+has name => (
+    is => 'ro',
+    isa => 'Str',
+);
+
+=head2 value
+
+ $string = $self->value;
+
+Value of the option.
+
+=cut
+
+has value => (
+    is => 'ro',
+    isa => 'Str',
+);
+
+=head2 quoted
+
+ $bool = $self->quoted;
+
+This flag tells if the option value should be quoted or not.
+
+=cut
+
+has quoted => (
+    is => 'ro',
+    isa => 'Bool',
+);
+
+=head1 METHODS
+
+=head2 captured_to_args
+
+=cut
+
+sub captured_to_args {
+    my $self   = shift;
+    my $name   = shift;
+    my $value  = shift;
+    my $quoted = 0;
+
+    $quoted = 1 if($value =~ s/^"(.*)"$/$1/g);
+
+    return {
+        name   => $name,
+        value  => $value,
+        quoted => $quoted,
+    };
+}
+
+=head1 AUTHOR
+
+See L<Net::DHCPd>.
+
+=cut
+
+1;
