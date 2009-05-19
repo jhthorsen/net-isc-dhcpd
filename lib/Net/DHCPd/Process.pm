b86b4bee605a0f5616656d0c96cbbe00aa8e2451
Jan Henning Thorsen <git@flodhest.net>
2009-05-20 00:27:26 +0200

    add minimum of process api

---
+++ lib/Net/DHCPd/Process.pm
@@ -0,0 +1,66 @@
+package Net::DHCPd::Process;
+
+=head1 NAME
+
+Net::DHCPd::Process
+
+=cut
+
+use strict;
+use warnings;
+
+=head1 METHODS
+
+=head2 new
+
+ $self = $class->new($args)
+ $self = $class->new(%args)
+
+Spawns a dhcpd process, running in the background.
+
+Args:
+
+ program
+ args
+ user
+ group
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $args  = @_ == 1 ? $_[0] : {@_};
+    my $self  = bless $args, $class;
+
+    # spawn...
+
+    return $self;
+}
+
+=head2 pid
+
+ $pid = $self->pid
+
+=cut
+
+sub pid {
+    return;
+}
+
+=head2 kill
+
+ $bool = $self->kill($signal)
+
+=cut
+
+sub kill {
+    return;
+}
+
+=head1 AUTHOR
+
+See L<Net::DHCPd>.
+
+=cut
+
+1;
