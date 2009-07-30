aff51d1fa90497c98181c2141c1760d1dab4ed4c
Jan Henning Thorsen <git@flodhest.net>
2009-07-30 02:17:07 +0200

    add meta class for attributes

---
+++ lib/Net/ISC/DHCPd/OMAPI/Meta/Attribute.pm
@@ -0,0 +1,50 @@
+package Net::ISC::DHCPd::OMAPI::Meta::Attribute;
+
+=head1 NAME
+
+Net::ISC::DHCPd::OMAPI::Meta::Attribute
+
+=cut
+
+use Moose::Role;
+
+=head1 ATTRIBUTES
+
+=head2 actions
+
+ $array_ref = $self->actions;
+
+Actions possible to execute on attribute. Can be:
+
+ lookup
+ examine
+ update
+
+=cut
+
+has actions => (
+    is => 'ro',
+    isa => 'ArrayRef',
+);
+
+=head1 METHODS
+
+=head2 has_action
+
+ $bool = $self->has_action($action_name);
+
+Returns true if the attribute can execute C<$action_name>.
+
+=cut
+
+sub has_action {
+    return grep { $_[1] eq $_ } @{ $_[0]->actions };
+}
+
+=head1 AUTHOR
+
+See L<Net::ISC::DHCPd>.
+
+=cut
+
+1;
