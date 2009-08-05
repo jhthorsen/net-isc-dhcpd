300cee25a0d2ec46c987161dccaeb722a0d84768
Jan Henning Thorsen <git@flodhest.net>
2009-08-05 15:29:02 +0200

    omapi_attr can take an array ref (like has())

--- lib/Net/ISC/DHCPd/OMAPI/Meta/Class.pm
+++ lib/Net/ISC/DHCPd/OMAPI/Meta/Class.pm
@@ -40,7 +40,7 @@ It will also set "coerce => 1", when "isa" is one of L<MOOSE TYPES>.
 
 sub omapi_attr {
     my $class = shift;
-    my $name  = shift;
+    my $names = ref $_[0] eq 'ARRAY' ? shift : [shift];
     my %opts  = @_;
 
     if(my $isa = $opts{'isa'}) {
@@ -50,12 +50,14 @@ sub omapi_attr {
         }
     }
 
-    $class->meta->add_attribute($name => (
-        is => 'rw',
-        predicate => "has_$name",
-        traits => [qw/Net::ISC::DHCPd::OMAPI::Meta::Attribute/],
-        %opts,
-    ));
+    for my $name (@$names) {
+        $class->meta->add_attribute($name => (
+            is => 'rw',
+            predicate => "has_$name",
+            traits => [qw/Net::ISC::DHCPd::OMAPI::Meta::Attribute/],
+            %opts,
+        ));
+    }
 }
 
 Moose::Exporter->setup_import_methods(
