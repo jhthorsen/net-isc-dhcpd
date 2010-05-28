76e28b270ce2a49515f771faaf4a5686a22560c6
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-05-28 23:40:22 +0200

    0.05_02  Fri May 28 23:40:21 CEST 2010        - Fix RT57947: Minor improvement for Net::ISC::DHCPd::OMAPI:          "key" not required for localhost          Contributor: Patrick

--- lib/Net/ISC/DHCPd.pm
+++ lib/Net/ISC/DHCPd.pm
@@ -6,7 +6,7 @@ Net::ISC::DHCPd - Interacts with ISC DHCPd
 
 =head1 VERSION
 
-0.05_01
+0.05_02
 
 =head1 SYNOPSIS
 
@@ -28,7 +28,7 @@ use File::Path;
 use File::Temp;
 use Net::ISC::DHCPd::Process;
 
-our $VERSION = '0.05_01';
+our $VERSION = '0.05_02';
 
 =head1 OBJECT ATTRIBUTES
 
--- t/00-load.t
+++ t/00-load.t
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use lib qw(lib);
 use Test::More;
 plan tests => 28;
