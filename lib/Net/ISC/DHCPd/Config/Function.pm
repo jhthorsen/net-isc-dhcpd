875f989083f083308beb6e45322b5f7c3c22cfe2
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-08-02 12:30:46 +0200

    add Function methods to manipulate function body

--- lib/Net/ISC/DHCPd/Config/Function.pm
+++ lib/Net/ISC/DHCPd/Config/Function.pm
@@ -37,52 +37,96 @@ has name => (
 =head2 body
 
  $text = $self->body
+ $int = $self->body_length;
+ $bool = $self->replace_body($pattern, $replacement);
+ $self->append_body($text);
+ $self->prepend_body($text);
 
-The body text of the function, without trailing newline.
+The body text of the function, without trailing newline at end.
 
 =cut
 
 has body => (
     is => 'rw',
     isa => 'Str',
-    trigger => sub { chomp shift->{'body'} },
+    traits => ['String'],
+    default => '',
+    handles => {
+        append_body => 'append',
+        prepend_body => 'prepend',
+        body_length => 'length',
+        replace_body => 'replace',
+    },
 );
 
-sub _build_children { [undef] }
+# need around modifier, since
+# trigger => sub { shift->_chomp_body },
+# results in recursion
+around body => sub {
+    my $next = shift;
+    my $self = shift;
 
+    if(my @text = @_) {
+        chomp @text;
+        return $self->$next(@text);
+    }
+
+    return $self->$next;
+};
+
+sub _build_children { [undef] }
 sub _build_regex { qr{^\s* on \s (\w+)}x }
 
 has _depth => (
-    is => 'rw',
-    isa => 'ScalarRef',
-    default => sub { my $i = 1; \$i },
+    is => 'ro',
+    isa => 'Int',
+    traits => ['Counter'],
+    default => 1,
+    handles => {
+        _inc_depth => 'inc',
+        _dec_depth => 'dec',
+    },
 );
 
 =head1 METHODS
 
+=head2 BUILD
+
+Will make sure L</body> does not contain trailing newlines.
+
+=cut
+
+sub BUILD {
+    $_[0]->body($_[0]->body);
+}
+
 =head2 slurp
+
+This method will slurp the content of the function, instead of
+trying to parse the statements.
 
 =cut
 
 sub slurp {
-    my $self  = shift;
-    my $line  = shift;
-    my $depth = $self->_depth;
+    my $self = shift;
+    my $line = shift;
 
-    $$depth++ if($line =~ /{/);
-    $$depth-- if($line =~ /}/);
+    $self->_inc_depth if($line =~ /{/);
+    $self->_dec_depth if($line =~ /}/);
 
-    if($$depth) {
-        $self->{'body'} .= $line;
-        return "next";
+    if($self->_depth) {
+        $self->append_body($line);
+        return 'next';
     }
     else {
-        chomp $self->{'body'};
-        return "last";
+        $self->body($self->body);
+        return 'last';
     }
 }
 
 =head2 captured_to_args
+
+See L<Net::ISC::DHCPd::Config::Role::captured_to_args()>.
 
 =cut
 
@@ -92,16 +136,12 @@ sub captured_to_args {
 
 =head2 generate
 
+See L<Net::ISC::DHCPd::Config::Role::generate()>.
+
 =cut
 
 sub generate {
-    my $self = shift;
-
-    return(
-        sprintf('on %s {', $self->name),
-        $self->body,
-        '}',
-    );
+    return sprintf 'on %s {', $_[0]->name), $_[0]->body, '}';
 }
 
 =head1 AUTHOR
