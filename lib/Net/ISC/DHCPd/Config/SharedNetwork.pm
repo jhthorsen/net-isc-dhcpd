a33eff7f94e99b2c6c4cc8cc9f66251afbd003e6
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-08-06 17:56:48 +0200

    shared-network can have a name

--- lib/Net/ISC/DHCPd/Config/SharedNetwork.pm
+++ lib/Net/ISC/DHCPd/Config/SharedNetwork.pm
@@ -9,6 +9,19 @@ Net::ISC::DHCPd::Config::SharedNetwork - Shared-network config parameter
 See L<Net::ISC::DHCPd::Config::Role> for methods and attributes without
 documentation.
 
+An instance from this class, comes from / will produce one of the
+blocks below, dependent on L</name> is set or not.
+    
+    shared-network $name_attribute_value {
+        $subnets_attribute_value
+        $keyvalues_attribute_value
+    }
+
+    shared-network {
+        $subnets_attribute_value
+        $keyvalues_attribute_value
+    }
+
 =head1 SYNOPSIS
 
 See L<Net::ISC::DHCPd::Config> for synopsis.
@@ -34,9 +47,20 @@ A list of parsed L<Net::ISC::DHCPd::Config::Subnet> objects.
 
 A list of parsed L<Net::ISC::DHCPd::Config::KeyValue> objects.
 
+=head2 name
+
+Holds a string representing the name of this shared network.
+Will be omitted if it contains an empty string.
+
 =cut
 
-sub _build_regex { qr{^\s* shared-network}x }
+has name => (
+    is => 'ro',
+    isa => 'Str',
+    default => '',
+);
+
+sub _build_regex { qr{^\s* shared-network \W* (\w*)}x }
 
 =head1 METHODS
 
@@ -47,9 +71,11 @@ See L<Net::ISC::DHCPd::Config::Role/generate>.
 =cut
 
 sub generate {
+    my $self = shift;
+
     return(
-        'shared-network {',
-        shift->generate_config_from_children,
+        'shared-network ' .($self->name ? $self->name . ' ' : '') . '{',
+        $self->generate_config_from_children,
         '}',
     );
 }
