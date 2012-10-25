65daff342bd4e95b4be03c2a849901bb53909ca2
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-10-25 19:49:38 +0200

    0.14        - [RT80374] Debug is enabled with DHCP_OMAPI_DEBUG=1        - [RT80373] Changed part of the documentation

--- lib/Net/ISC/DHCPd.pm
+++ lib/Net/ISC/DHCPd.pm
@@ -6,7 +6,7 @@ Net::ISC::DHCPd - Interacts with ISC DHCPd
 
 =head1 VERSION
 
-0.13
+0.14
 
 =head1 SYNOPSIS
 
@@ -41,7 +41,7 @@ use MooseX::Types::Path::Class qw(File);
 use Net::ISC::DHCPd::Types ':all';
 use File::Temp;
 
-our $VERSION = eval '0.13';
+our $VERSION = eval '0.14';
 
 =head1 ATTRIBUTES
 
