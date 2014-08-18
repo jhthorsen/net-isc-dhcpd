32e6512b16258c64a3964359f244a27a8de1caf1
Robert Drake <rdrake@direcpath.com>
2014-08-18 19:42:34 -0400

    subnets can contain classes

--- lib/Net/ISC/DHCPd/Config/Subnet6.pm
+++ lib/Net/ISC/DHCPd/Config/Subnet6.pm
@@ -42,6 +42,7 @@ sub children {
         Net::ISC::DHCPd::Config::Range6
         Net::ISC::DHCPd::Config::Filename
         Net::ISC::DHCPd::Config::Option
+        Net::ISC::DHCPd::Config::Class
         Net::ISC::DHCPd::Config::KeyValue
         Net::ISC::DHCPd::Config::Block
         /;
