2da554e8b4570db0097511ca21adbceba9353777
Robert Drake <rdrake@direcpath.com>
2016-02-03 13:49:52 -0500

    version bump to 0.1709
    new release because we haven't done one in a while and we're putting in
    fixes.
    
    Hopefully this won't break anything.

--- lib/Net/ISC/DHCPd.pm
+++ lib/Net/ISC/DHCPd.pm
@@ -6,7 +6,7 @@ Net::ISC::DHCPd - Interacts with ISC DHCPd
 
 =head1 VERSION
 
-0.1708
+0.1709
 
 =head1 SYNOPSIS
 
@@ -43,7 +43,7 @@ use Net::ISC::DHCPd::Types ':all';
 use File::Temp 0.20;
 use v5.10.1;
 
-our $VERSION = eval '0.1708';
+our $VERSION = eval '0.1709';
 
 =head1 ATTRIBUTES
 
